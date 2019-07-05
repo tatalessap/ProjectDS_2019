791aa7c627b3f4965eaa111b38de1797c911551a
Confiteor <ronco95ca@gmail.com>
2019-07-05 18:52:16 +0200

    Road Class

--- core/classes.py
+++ core/classes.py
@@ -2,33 +2,63 @@ import math
 
 
 class Node:
-    def __init__(self, index, x, y, visited, type, quantity):
+    def __init__(self, index, x, y, visited, nodetype, quantity):
         self.index = index
         self.x = x
         self.y = y
         self.visited = visited
-        self.type = type
-        self.quantity = quantity;
+        self.type = nodetype
+        self.quantity = quantity
 
 
 class Arc:
     def __init__(self, node1, node2, deposit):
-        self.node1 = node1
-        self.node2 = node2
-        self.cost = math.sqrt((node1.x-node2.x)**2 + (node1.y-node2.y)**2)
-        self.saving = math.sqrt((node1.x - deposit.x)**2+(node1.y-deposit.y)**2) + math.sqrt((deposit.x - node2.x)**2 + (deposit.y - node2.y)**2) - self.cost
+        self.nodes = [node1, node2]
+        self.cost = math.sqrt((node1.x - node2.x) ** 2 + (node1.y - node2.y) ** 2)
+        self.saving = math.sqrt((node1.x - deposit.x) ** 2 + (node1.y - deposit.y) ** 2) + math.sqrt((deposit.x - node2.x) ** 2 + (deposit.y - node2.y) ** 2) - self.cost
         self.arcType = node1.type + node2.type
 
 
-    def getCost(self):
-        print(self.cost)
-
-    def getSaving(self):
-        return self.saving
-
-    def getArcType(self):
-        return self.arcType
-
-
 class Route:
-    pass
+    def __init__(self, index, capacity):
+        self.index = index
+        self.totalCost = 0
+        self.capacity = capacity
+        self.load = 0
+        self.route = []
+        self.indexTale = 0
+        self.indexHead = 0
+
+    def firstAdd(self, arc):
+        if arc.nodes[0].quantity + arc.nodes[1].quantity > self.capacity:
+            return False
+
+        self.route.append(arc.nodes[0])
+        self.indexTale = arc.nodes[0].index
+        self.route.append(arc.nodes[1])
+        self.indexHead = arc.nodes[1].index
+
+        arc.nodes[0].visited = True
+        arc.nodes[1].visited = True
+
+        self.totalCost = arc.cost
+        self.load = arc.nodes[0].quantity + arc.nodes[1].quantity
+
+        return True
+
+    def add(self, arc, n, position):
+        if self.load + arc.nodes[n].quantity > self.capacity:
+            return False
+
+        if position == "h":
+            self.route.append(arc.nodes[n])
+            self.indexHead = arc.nodes[n].index
+        if position == "t":
+            self.route.insert(0, arc.nodes[n])
+            self.indexTale = arc.nodes[n].index
+
+        arc.nodes[n].visited = True
+        self.totalCost = self.totalCost + arc.cost
+        self.load = self.load + arc.nodes[n].quantity
+
+        return True
