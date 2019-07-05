54a889df48fa74e0a0cfb1e283542cec76c8f4dd
Confiteor <ronco95ca@gmail.com>
2019-07-05 16:35:36 +0200

    Read Istance

--- core/main.py
+++ core/main.py
@@ -1,4 +1,13 @@
 from classes import *
 from toReadFile import *
 
+node1 = Node(1, 1, 1, False, "L", 3)
+node2 = Node(2, 2, 2, False, "L", 3)
+deposit = Node(0,0,0, False, "D", 0)
+arc = Arc (node1, node2, deposit)
+
 nodesL, nodesB, arcsL, arcsB, nCustomers, deposit, vehiclesCapacity, nVehicles = ReadIstance("istances\A1.txt")
+
+#for i in range(len(arcsL)):
+   # print("\n"+str(i)+"    "+str(arcsL[i].saving))
+
