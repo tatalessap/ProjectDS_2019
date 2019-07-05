7cc2556c2926420119cfa5a50802494967bbba34
Confiteor <ronco95ca@gmail.com>
2019-07-05 11:53:45 +0200

    Sintax fixes

--- core/main.py
+++ core/main.py
@@ -1,6 +1,6 @@
 from classes import *
-nodo1 = Node(4,2,2,'false')
-nodo2 = Node(5,1,1,'false')
-deposit = Node(0,0,0,'false')
-arco = Arc(nodo1,nodo2,deposit)
+nodo1 = Node(4, 2, 2, 'false')
+nodo2 = Node(5, 1, 1, 'false')
+deposit = Node(0, 0, 0, 'false')
+arco = Arc(nodo1, nodo2, deposit)
 print(arco.getSaving())
